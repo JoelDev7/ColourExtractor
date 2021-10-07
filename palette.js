fc087496c7fe3a8e824f140a9d07beb7ed54fb5d
unknown <olivergarciaw7.0@gmail.com>
2021-10-06 22:58:37 -0500

    Palette section is almost complete

--- palette.js
+++ palette.js
@@ -1,11 +1,18 @@
+let start;
+let end;
 function palette(startColor, endColor, numColors) {
+    start = startColor;
+    end = endColor;
     let colour1 = document.getElementById('colour1')
     let colour2 = document.getElementById('colour2')
     colour1.style.backgroundColor = startColor;
     colour2.style.backgroundColor = endColor;
-    //console.log(startColor, endColor)
     let palette = chroma.scale([`${startColor}`, `${endColor}`]).mode('lch').colors(numColors)
     let container = document.querySelector('.palette-container')
+    const element = document.querySelectorAll('.color-holder')
+    element.forEach((element) => {
+        element.remove()
+    })
     palette.map((color) => {
         let colorHolder = document.createElement('div')
         colorHolder.style.width = `${100 / numColors}%`
@@ -17,5 +24,15 @@ function palette(startColor, endColor, numColors) {
 }
 
 window.onload = () => {
-    palette('#593F62', '#009FB7', 8)
+    palette('#593F62', '#009FB7', 6)
+}
+
+function elementExist() {
+    const element = document.querySelectorAll('.color-holder')
+    console.log(element.length)
+}
+
+function selected() {
+    const num = document.getElementById('number').value
+    palette(start, end, num)
 }
